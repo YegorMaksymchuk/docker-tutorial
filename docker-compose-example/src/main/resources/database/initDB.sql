1ba097fa71281076feae28378da9e60cd4eff696
Yegor Maksymchyk <ymaksymc@redhat.com>
2018-03-14 21:23:05 +0200

    refactoring

--- docker-compose-example/src/main/resources/database/initDB.sql
+++ docker-compose-example/src/main/resources/database/initDB.sql
@@ -1,5 +1,9 @@
-CREATE SCHEMA IF NOT EXISTS restdemo
+DROP SCHEMA IF EXISTS restdemo;
+
+CREATE SCHEMA restdemo
   DEFAULT CHARACTER SET utf8mb4;
+
+DROP TABLE IF EXISTS restdemo.customers;
 
 CREATE TABLE IF NOT EXISTS restdemo.customers (
   id         BIGINT PRIMARY KEY,
